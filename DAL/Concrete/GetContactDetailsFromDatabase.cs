fba3b131bc3eab843d2a7e215d5a4b9d554ce139
mickos1972 <mickm15@hotmail.com>
2020-08-08 10:47:25 +0100

    Repaced with real data setup.

--- DAL/Concrete/GetContactDetailsFromDatabase.cs
+++ DAL/Concrete/GetContactDetailsFromDatabase.cs
@@ -7,53 +7,7 @@ using Microsoft.EntityFrameworkCore;
 namespace DAL
 {
     public class GeContactDetailsFromDatabase : IGetContactDetailsFromDatabase
-    {
-        public GeContactDetailsFromDatabase()
-        {
-            //Some Initial setup data
-            //using CVContext context = new CVContext();
-
-            //var skill = new Skills()
-            //{
-            //    Description = "c++"
-            //};
-
-            //context.Add(skill);
-            //context.SaveChanges();
-
-            //var bobba = new Contact()
-            //{
-            //    firstName = "bobba",
-            //    lastName = "fett",
-            //    emailAddress = "bounter@hunters.com",
-            //    SkillId = 1,
-            //    WorkExperiences = new List<WorkExperienceDAL>()
-            //};
-
-            //context.Add(bobba);
-            //context.SaveChanges();
-
-            //var work = new WorkExperienceDAL()
-            //{
-            //    from = DateTime.Today,
-            //    to = DateTime.Today.AddDays(6),
-            //    duties = "hunting smugglers",
-            //    ContactId = 1
-            //};
-            //context.Add(work);
-            //context.SaveChanges();
-
-            //var work2 = new WorkExperienceDAL()
-            //{
-            //    from = DateTime.Today,
-            //    to = DateTime.Today.AddDays(3),
-            //    duties = "looking cool",
-            //    ContactId = 1
-            //};
-            //context.Add(work2);
-            //context.SaveChanges();
-        }
-
+    {       
         public Contact getContactDetailsById(int id)
         {
             try
